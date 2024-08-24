58a49d70abb3c29dc12f94ab016984d1ebc92608
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-08-24 23:35:38 +0300

    sets up DbContext for managing and accessing data

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -1,9 +1,14 @@
 
+using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
-public class GameStoreContext : DbContext
+// DbContext tracks changes to entity objects, allowing you to perform CRUD operations (Create, Read, Update, Delete) and persist these changes to the database.
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
 {
-
+    public DbSet<Game> Games => Set<Game>(); //defines a property in the DbContext that represents a collection of Game entities.
+    public DbSet<Genre> Genres => Set<Genre>();
 }
+
+//DbSet<Game>: DbSet<T> is a special class in Entity Framework Core that represents a collection of entities of type T (in this case, Game). It allows you to perform CRUD operations (Create, Read, Update, Delete) on entities in the database.
