325366c13492868adc05a39b17f0d09c74a1abd5
Hannah Nguyen <myhanh.nguyen.969317598@gmail.com>
2024-08-24 21:54:26 +0300

    create api endpoints, use extension method MapGameEndpoints

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,94 @@
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+// use static class because no need to create new object from class later. it's just class defining endpoints
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGameById";
+    private static readonly List<GameDto> games = [
+    new(
+     1,
+     "Final Fantasy XIV",
+    "RolePlaying",
+    9.99M,
+    new DateOnly(1992,7,14)),
+    new(
+     2,
+     "Street fighter XIV",
+    "Fighting",
+    19.85M,
+    new DateOnly(1998,9,24)),
+     new(
+     3,
+     "Chicken Island",
+    "Fighting",
+    34.9M,
+    new DateOnly(2009,10,14)),
+     new(
+     4,
+     "Dragon Balls",
+    "Action",
+    300,
+    new DateOnly(2016,2,16))
+
+    ];
+
+    //this WebApplication app: The this keyword here indicates that MapGameEndpoints is an extension method for the WebApplication class. Extension methods allow you to "add" methods to existing types without modifying the original type or creating a new derived type.
+    public static WebApplication MapGameEndpoints(this WebApplication app)
+    {
+        //GET
+        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/games", () => games);
+        app.MapGet("/games/{id}", (int id) =>
+        {
+            GameDto? foundGame = games.Find(game => game.Id == id);
+            return foundGame is null ? Results.NotFound() : Results.Ok(foundGame);
+        }).WithName(GetGameEndpointName);
+
+        //POST
+        app.MapPost("/games", (CreateGameDto newGame) =>
+        {
+            GameDto game = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+            games.Add(game);
+
+            return Results.CreatedAtRoute(
+                GetGameEndpointName,
+                new { id = game.Id },
+                game
+                );
+        });
+
+        //UPDATE GAME/ PUT
+        app.MapPut("/games/{id}", (int id, UpdateGameDto updateGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1) return Results.NotFound();
+            games[index] = new GameDto(
+               id,
+               updateGame.Name,
+               updateGame.Genre,
+               updateGame.Price,
+               updateGame.ReleaseDate
+               );
+
+            return Results.NoContent();
+        });
+
+        // DELETE
+        app.MapDelete("/games/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+        return app;
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+create gitignore file: dotnet new gitignore
+
+Command to run project: dotnet run
